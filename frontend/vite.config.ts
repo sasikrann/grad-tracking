7f785f8ea11ca9098323795bc44a15b48cef7294
sasikrann <sasikransawangtemm@gmail.com>
2026-06-22 19:33:00 +0700

    refactor: remove unused vueDevTools plugin from Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
     tailwindcss(),
   ],
   resolve: {
